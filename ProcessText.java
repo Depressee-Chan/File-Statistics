14c462147d20bdbd0d9dfd13dfe1aa7ece0bbd32
Nick C <60305140+Depressee-Chan@users.noreply.github.com>
2021-03-11 12:58:27 -0700

    Add files via upload

---
+++ ProcessText.java
@@ -0,0 +1,9 @@
+package fileSearcher;
+
+public class ProcessText {
+	
+	//main method
+	public static void main(String[] args){
+		
+	}
+}
---
+++ TextStatistics.java
@@ -0,0 +1,54 @@
+package fileSearcher;
+//import java.io.File;
+//import java.util.Arrays;
+
+public class TextStatistics implements TextStatisticsInterface{
+	
+	//main method
+	public static void main(String[] args){
+		System.out.println("hey it does something!");
+	}
+
+	//gets num of chars in file
+	@Override
+	public int getCharCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//gets num of words in file
+	@Override
+	public int getWordCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//gets num of lines in file
+	@Override
+	public int getLineCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//gets num of letters in file
+	@Override
+	public int[] getLetterCount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//gets length of words in file
+	@Override
+	public int[] getWordLengthCount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//gets average length of a word in the file
+	@Override
+	public double getAverageWordLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
