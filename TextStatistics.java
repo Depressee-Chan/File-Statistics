5be768e1db35bf9216b54b84777e8ea4b5cc0ff6
Nick C <60305140+Depressee-Chan@users.noreply.github.com>
2021-03-16 13:06:07 -0600

    Update TextStatistics.java

--- TextStatistics.java
+++ TextStatistics.java
@@ -1,54 +1,101 @@
 package fileSearcher;
-//import java.io.File;
-//import java.util.Arrays;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TextStatistics implements TextStatisticsInterface{
 	
-	//main method
-	public static void main(String[] args){
-		System.out.println("hey it does something!");
+	private File textFile;
+	private int charCount;
+	private int wordCount;
+	private int lineCount;
+	private int[] letterCount;
+	private int[] wordLength;
+	private double averageWordLength;
+	private static final String DELIMITERS = "[\\W\\d_]+";
+	
+	public TextStatistics(File textFile) {
+		// TODO Auto-generated constructor stub
+		this.textFile = textFile;
 	}
 
 	//gets num of chars in file
 	@Override
 	public int getCharCount() {
 		// TODO Auto-generated method stub
-		return 0;
+	    try {
+	    	//creates scanner object for file object
+	        Scanner myReader = new Scanner(textFile);
+	        //while there is a next line in scanner execute code within
+	        while (myReader.hasNextLine()) {
+	        	//count chars in line and add num to charCount
+	            charCount += myReader.nextLine().length();
+	            
+	        }
+	        //close connection to file
+	        myReader.close();
+	    } catch (FileNotFoundException e) {
+	    	//if error, print error message
+	        System.out.println("An error occurred.");
+	    }
+	    
+	    //return charCount
+	    return charCount;
 	}
 
 	//gets num of words in file
 	@Override
 	public int getWordCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return wordCount;
 	}
 
 	//gets num of lines in file
 	@Override
 	public int getLineCount() {
 		// TODO Auto-generated method stub
-		return 0;
+	    try {
+	    	//creates scanner object for file object
+	        Scanner myReader = new Scanner(textFile);
+	        //while there is a next line in scanner execute code within
+	        while (myReader.hasNextLine()) {
+	        	//adds one to the line count
+	        	lineCount += 1;
+	        	myReader.nextLine();
+	        }
+	        //close connection to file
+	        myReader.close();
+	      } catch (FileNotFoundException e) {
+	    	//if error, print error message
+	        System.out.println("An error occurred.");
+	      }
+	    
+	    //return lineCount
+	    return lineCount;
 	}
 
 	//gets num of letters in file
 	@Override
 	public int[] getLetterCount() {
 		// TODO Auto-generated method stub
-		return null;
+		return letterCount;
 	}
 
 	//gets length of words in file
 	@Override
 	public int[] getWordLengthCount() {
 		// TODO Auto-generated method stub
-		return null;
+		return wordLength;
 	}
 
 	//gets average length of a word in the file
 	@Override
 	public double getAverageWordLength() {
 		// TODO Auto-generated method stub
-		return 0;
+		return averageWordLength;
 	}
 	
+	public String toString(){
+		return "oh boy!" + getLineCount();
+	}
 }
