fd01eafa030f2f26eb387d25337d2330a7587bd7
Nick C <60305140+Depressee-Chan@users.noreply.github.com>
2021-03-16 13:06:29 -0600

    Update ProcessText.java

--- ProcessText.java
+++ ProcessText.java
@@ -1,9 +1,27 @@
 package fileSearcher;
 
+import java.io.*;
+
 public class ProcessText {
-	
 	//main method
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		for(String arg : args){
+			//gets current working directory as a string
+			String currentDir = System.getProperty("user.dir") + "/src/fileSearcher/";
+			//creates a text file object out of the given file name(s) and current working directory 
+			File textFile = new File(currentDir + arg);
+			
+			//checks if file object is actually a file
+			if(textFile.exists() && textFile.isFile()) {
+				//creates new TextStatistics object with textFile object
+				TextStatistics info = new TextStatistics(textFile);
+				//calls toString method and prints out TextStatistics object
+				System.out.println(info.toString());
+				
+			}else{
+				//if error with file not being found/detected, print an error message
+				System.out.println("Try entering a valid text file ending in .txt");
+			}
+		}
 	}
 }
